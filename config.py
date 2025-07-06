8f7d9c11e9a3c58cc1d5d328a3e8473e1af1a9ba
acxanime <acxanime07@gmail.com>
2025-07-06 18:37:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Anime_Chats_St")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/9eff9023429a2bf643574-add775ea5ee45e9d24.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/7fd0b023e49292d657ffd-62b41dad1db226fc77.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/eb7c8ae9786956240d9f6-48d8610b8c5263ba0e.jpg")
 
 #--------------------------------------------
